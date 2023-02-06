3ff797222bd8e88842a4fde776932b57416ca9b2
ZhijunLin7 <zlin1@cifpfbmoll.eu>
2023-02-06 22:28:25 +0100

    "Hecho"

--- app/src/main/java/com/example/projectedb/MainActivity.java
+++ app/src/main/java/com/example/projectedb/MainActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.example.projectedb.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private SqlClass sql;
+    private  String titol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +26,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         sql = new SqlClass(this);
+
+        refrescar();
+
         binding.buttonCrear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Comentario c = new Comentario(binding.titol.getText().toString(),binding.comentario.getText().toString());
                 sql.addComent(c);
+                binding.titol.setText("");
+                binding.comentario.setText("");
 
+                refrescar();
             }
         });
 
         binding.buttonver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ArrayList<Comentario> comentarios=sql.getComentarios();
-                for (int i = 0; i <comentarios.size() ; i++) {
-                    Log.d("testeo", "onClick: "+comentarios.get(i).toString());
-                }
+                Comentario c=sql.getComentario(titol);
+                binding.titolver.setText(c.getTitol());
+                binding.comentariover.setText(c.getComentario());
+
             }
         });
+
+        binding.buttoneliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sql.eliminar(titol);
+                titol="";
+                binding.titolver.setText("");
+                binding.comentariover.setText("");
+
+                refrescar();
+            }
+        });
+
+        binding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                titol=adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    }
+    public void refrescar(){
+        ArrayList<String> comentarios=sql.getComentarios();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, comentarios);
+        binding.spinner.setAdapter(adapter);
     }
 }
